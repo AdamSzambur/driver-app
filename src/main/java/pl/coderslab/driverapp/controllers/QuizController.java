1034b82c61b9d5c0ef463ba67421336c60692314
AdamSzambur <adam.szamburski@gmail.com>
2019-11-12 22:54:19 +0100

    Add spring security

--- src/main/java/pl/coderslab/driverapp/controllers/QuizController.java
+++ src/main/java/pl/coderslab/driverapp/controllers/QuizController.java
@@ -15,7 +15,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/quizzes")
+@RequestMapping("admin/quizzes")
 public class QuizController {
 
     private QuizService quizService;
